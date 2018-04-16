a6f61e7705e9381a9cd1fd26b1966a122aa80d4a
sp <q18600519285@gmail.com>
2018-04-16 10:28:32 +0800

    忘记密码

--- app/src/main/java/com/example/lc/computer/ForgetActivity.java
+++ app/src/main/java/com/example/lc/computer/ForgetActivity.java
@@ -1,10 +1,12 @@
 package com.example.lc.computer;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -23,26 +25,51 @@ public class ForgetActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget);
-
+        StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        intview();
     }
     private void intview(){
         forget_id =(EditText)findViewById(R.id.forget_id);
         forget_name =(EditText)findViewById(R.id.forget_name);
-        forget_password =(Button)findViewById(R.id.forget_pwd);
+        forget_password =(Button)findViewById(R.id.getpassowrd);
         main_return =(Button)findViewById(R.id.main_return);
+        forget_password.setOnClickListener(this);
+        main_return.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.forget_pwd:
+            case R.id.getpassowrd:
+                String name=null,password = null;
+                String id =forget_id.getText().toString();
+                String Fname =forget_name.getText().toString().trim();
                 connection = SqlHelper.openConnection();
                 try{
                     statement = connection.createStatement();
+                    resultSet = statement.executeQuery("SELECT * FROM student WHERE studentID = "+id+"");
+                    if (resultSet.next()){
+                        name = resultSet.getString("name");
+                        password = resultSet.getString("password");
+                        if (Fname.equals(name)){
+                            Toast.makeText(this,"您的密码是"+ password +"",Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(this,"信息有误",Toast.LENGTH_LONG).show();
+                        }
+                    }else {
+                        Toast.makeText(this,"不存在的学号",Toast.LENGTH_LONG).show();
+                    }
 
                 }catch (SQLException e){
 
                 }
+                break;
+            case R.id.main_return:
+                finish();
+                break;
+                default:
+                    break;
         }
     }
 }
